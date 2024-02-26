925f972f9bda1defea733dcf38f33a67b01c50d7
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-02-26 13:09:21 -0500

    algoritmoEncontrarNumeroMayorDeTresNumeros

---
+++ algoritmoEncontrarNumeroMayorDeTresNumeros.py
@@ -0,0 +1,12 @@
+A = int(input("Introduzca un numero: "))
+B = int(input("Introduzca un numero: "))
+C = int(input("Introduzca un numero: "))
+
+if A > B and A > C:
+    print("El numero mayor es:",A)
+    
+elif B > A and B > C:
+    print("El numero mayor es:",B)
+    
+elif C > A and C > B:
+    print("El numero mayor es:",C)
